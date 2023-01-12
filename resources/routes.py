4ea70c5fd9c251ca018cad4bf919e917d07d9bbe
melbesnard85 <melbesnard85@gmail.com>
2023-01-13 00:31:07 +0900

    feat: changes routes

--- resources/routes.py
+++ resources/routes.py
@@ -1,6 +1,6 @@
 from .auth import SignupApi, LoginApi, RefreshApi
 
 def initialize_routes(api):
-    api.add_resource(SignupApi, '/auth/signup')
-    api.add_resource(LoginApi, '/auth/login')
-    api.add_resource(RefreshApi, 'auth/refesh')
+    api.add_resource(SignupApi, '/api/create_user')
+    api.add_resource(LoginApi, '/api/get_token')
+    api.add_resource(RefreshApi, '/api/refresh')
