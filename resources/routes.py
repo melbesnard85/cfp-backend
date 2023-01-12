3ee9a300992cd7a111031b5607ad0db61d05aa7d
melbesnard85 <melbesnard85@gmail.com>
2023-01-13 01:29:24 +0900

    fix: change refresh api to is_token_valid

--- resources/routes.py
+++ resources/routes.py
@@ -3,4 +3,4 @@ from .auth import SignupApi, LoginApi, RefreshApi
 def initialize_routes(api):
     api.add_resource(SignupApi, '/api/create_user')
     api.add_resource(LoginApi, '/api/get_token')
-    api.add_resource(RefreshApi, '/api/refresh')
+    api.add_resource(RefreshApi, '/api/is_token_valid')
