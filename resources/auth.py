6488d227915ae50ac1e64b1a42fb4f399c31fc89
melbesnard85 <melbesnard85@gmail.com>
2023-01-12 23:17:35 +0900

    feat: implement api auth routes

--- resources/auth.py
+++ resources/auth.py
@@ -1,55 +1,30 @@
 from flask import Blueprint, request, make_response, render_template, redirect, url_for
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, get_jwt, set_access_cookies, jwt_required
+from flask_restful_swagger_2 import Resource
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, get_jwt, set_access_cookies, create_refresh_token
 from database.models import User
 import datetime
 
-auth = Blueprint('auth', __name__, template_folder='templates')
-
-@auth.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        user = User(**request.form)
+class SignupApi(Resource):
+    def post(self):
+        user = User(**request.get_json())
         user.hash_password()
         user.save()
-        accessToken = create_access_token(identity=str(user.id), expires_delta=datetime.timedelta(days=1))
-        resp = make_response(redirect(url_for('auth.dashboard'))) # Redirect to wherever after login
-        resp.set_cookie('access_token_cookie', accessToken, max_age=60*60*24) # Expires in 1 day
-        return resp
-    else:
-        if request.cookies.get('access_token_cookie'):
-            return redirect(url_for('auth.dashboard'))
-        return render_template('signup.html') # Our signup page
+        return { 'id': str(user.id) }
 
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        body = request.form
+class LoginApi(Resource):
+    def post(self):
+        body = request.get_json()
         user = User.objects.get(email=body['email'])
         authorized = user.check_password(body['password'])
         if not authorized:
-            return render_template('login.html', failed=True) # Our login page
+            return 'Unauthorized Error', 401
         accessToken = create_access_token(identity=str(user.id), expires_delta=datetime.timedelta(days=1))
-        resp = make_response(redirect(url_for('auth.dashboard'))) # Redirect to wherever after login
-        resp.set_cookie('access_token_cookie', accessToken, max_age=60*60*24) # Expires in 1 day
-        return resp
-    else:
-        if request.cookies.get('access_token_cookie'):
-            return redirect(url_for('auth.dashboard'))
-        return render_template('login.html') # Our login page
+        refreshToken = create_refresh_token(identity=str(user.id), expires_delta=datetime.timedelta(days=30))
+        return { 'accessToken': accessToken, 'refreshToken': refreshToken }
 
-@auth.after_app_request
-@jwt_required(optional=True)
-def refresh(response):
-    identity = get_jwt_identity()
-    if identity:
-        expTimestamp = get_jwt()['exp']
-        now = datetime.datetime.now()
-        targetTimestamp = datetime.datetime.timestamp(now + datetime.timedelta(minutes=30))
-        if targetTimestamp > expTimestamp:
-            accessToken = create_access_token(identity=identity, expires_delta=datetime.timedelta(days=1))
-            set_access_cookies(response, accessToken, max_age=60*60*24)
-    return response
-
-@auth.route('/dashboard', methods=['GET'])
-def dashboard():
-    return render_template('dashboard.html')
+class RefreshApi(Resource):
+    @jwt_required(refresh=True)
+    def get(self):
+        accessToken = create_access_token(identity=get_jwt_identity(), expires_delta=datetime.timedelta(days=1))
+        refreshToken = create_refresh_token(identity=get_jwt_identity(), expires_delta=datetime.timedelta(days=30))
+        return { 'accessToken': accessToken, 'refreshToken': refreshToken }
